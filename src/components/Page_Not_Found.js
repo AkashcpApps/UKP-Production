a45c73ec528a49d61a264ffd4f97efe172065160
AKASH CP <akashcpapps@gmail.com>
2021-11-05 18:11:58 +0530

    user and admin register changes

--- src/components/Page_Not_Found.js
+++ src/components/Page_Not_Found.js
@@ -1,11 +1,12 @@
 import React from "react";
 import page_construction from "../images/page_construction.png";
+import construction from '../images/under_construction.gif';
 
 export default function Page_Not_Found() {
   return (
     <div>
         <center>
-        <img src={page_construction} width="700" height="500" />
+        <img style={{margin:16}} src={construction} width="500" height="400" />
         </center>
     
     </div>
--- src/webpages/Register.js
+++ src/webpages/Register.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from '@material-ui/core/styles'
+import {makeStyles} from '@material-ui/core/styles'
 import FormLabel from "@material-ui/core/FormLabel";
 import FormControl from "@material-ui/core/FormControl";
 import FormGroup from "@material-ui/core/FormGroup";
@@ -9,178 +9,178 @@ import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import axios from "axios";
 import * as Constants from "../utils/Constants";
-import { ToastContainer, toast } from "react-toastify";
+import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+// import FormLabel from '@mui/material/FormLabel';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(2),
-      width: "25ch",
+    root: {
+        "& .MuiTextField-root": {
+            margin: theme.spacing(2),
+            width: "25ch"
+        }
     },
-  },
-  formControl: {
-    margin: theme.spacing(3),
-  },
+    formControl: {
+        margin: theme.spacing(3)
+    }
 }));
 
 export default function RegistrationForm() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    devAdmin: false,
-    admin: false,
-    dataEntry: false,
-  });
-  const[RoleID ,setRoleID]=React.useState('');
+    const classes = useStyles();
+    const [value, setValue] = React.useState('');
+    const [state, setState] = React.useState({devAdmin: false, admin: false, dataEntry: false});
+    const [RoleID, setRoleID] = React.useState('');
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
-    const checked = event.target.checked;
-
-    // to get the checked value
-    const checkedValue = event.target.value;
-
-    // to get the checked name
-    const checkedName = event.target.name;
-    setRoleID(checkedName);
-   // alert(checked + " " + checkedValue + " " + checkedName);
-  };
-
-  const handleIconClicks = (name) => () => {
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let emailId = document.getElementById("emailId").value;
-    let mobileNumber = document.getElementById("mobileNumber").value;
-    let userName = document.getElementById("userName").value;
-    let Password = document.getElementById("password").value;
-    let cpassword = document.getElementById("confirm-password").value;
-    var MyDate = new Date();
-    var MyDateString= ('0' + MyDate.getDate()).slice(-2) + '/'
-    + ('0' + (MyDate.getMonth()+1)).slice(-2) + '/'
-    + MyDate.getFullYear();
-
-   // alert(MyDateString);
-
-
-    // let date =
-    //   new Date().getDate() +
-    //   "/" +
-    //   new Date().getMonth()+''.length==1: +
-    //   "/" +
-    //   new Date().getFullYear();
-    if (Password === cpassword ) {
-
-      alert(firstName+' '+lastName+' '+RoleID+' '+emailId+' '+parseInt(mobileNumber)+' '+MyDateString+' '+userName);
-        if( (Password!=null && Password!=''))
-        {
-      axios
-        .post(Constants.url+'UKP/rest/endpoints/UserRegistration',
-        {
-          "FirstName": firstName,
-          "MiddleName": "MiddleName",
-          "LastName": lastName,
-          "RoleID": RoleID,
-          "Level": "Level",
-          "Email": emailId,
-          "EmailConfirmed": 0,
-          "PasswordHash": "PasswordHash",
-          "SecurityStamp": "SecurityStamp",
-          "PhoneNumber":mobileNumber,
-          "PhoneNumberConfirmed": 0,
-          "TwoFactorEnabled": 0,
-          "LockoutEndDateUtc": "10/11/1985",
-          "LockoutEnabled": 0,
-          "AccessFailedCount": 2,
-          "UserName": userName,
-          "VerificationCode": "12354",
-          "VerificationCodeConfirmed": 0
-        })
-        .then((res) => {
-          if (res.data.status == true && res.data.statusCode == 200) {
-            toast.success("Successfully Registered");
-          } else {
-            toast.error("Failed");
-          }
+    const handleChange = (event) => {
+        setState({
+            ...state,
+            [event.target.name]: event.target.checked
         });
-    }else{
-        toast.error('Enter all the fields');
-    } }else {
-      toast.error("Different password");
-    }
-  };
+        const checked = event.target.checked;
 
-  const { devAdmin, admin, dataEntry } = state;
+        // to get the checked value
+        const checkedValue = event.target.value;
 
-  return (
-    <div className="new-registraion-form">
-      <form className={classes.root} noValidate autoComplete="off">
-        <div>
-          <TextField required id="firstName" label="First Name" />
-          <TextField required id="lastName" label="Last Name" />
-          <TextField required id="emailId" label="Email" />
-          <TextField id="mobileNumber" label="Mobile Number" />
-        </div>
-        <div>
-          <TextField required id="userName" label="User Name" />
-          <TextField required id="password" label="Password" type="password" />
-          <TextField
-            required
-            id="confirm-password"
-            label="Confirm Password"
-            type="password"
-          />
-        </div>
-        <div className="new-registraion-form-role">
-          <h4>User Role</h4>
-        </div>
-        <div>
-          <FormControl component="fieldset" className={classes.formControl}>
-            <FormLabel component="legend">Assign Role</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={devAdmin}
-                    onChange={handleChange}
-                    name="devAdmin"
-                  />
+        // to get the checked name
+        const checkedName = event.target.name;
+        setRoleID(checkedName);
+        // alert(checked + " " + checkedValue + " " + checkedName);
+    };
+
+    const handleRadioChange = (event) => { // alert(event.target.value);
+        setValue(event.target.value);
+        setRoleID(event.target.value);
+    };
+
+    const cleraFiled = () => {
+      let firstName = document.getElementById("firstName").value='';
+      let lastName = document.getElementById("lastName").value='';
+      let emailId = document.getElementById("emailId").value='';
+      let mobileNumber = document.getElementById("mobileNumber").value='';
+      let userName = document.getElementById("userName").value='';
+      let Password = document.getElementById("password").value='';
+      let cpassword = document.getElementById("confirm-password").value='';
+    };
+
+    const handleIconClicks = (name) => () => {
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let emailId = document.getElementById("emailId").value;
+        let mobileNumber = document.getElementById("mobileNumber").value;
+        let userName = document.getElementById("userName").value;
+        let Password = document.getElementById("password").value;
+        let cpassword = document.getElementById("confirm-password").value;
+      
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0');
+        var yyyy = today.getFullYear();
+        today = dd + '/' + mm + '/' + yyyy;
+        if (Password === cpassword) {
+         //   alert(firstName + ' ' + lastName + ' ' + RoleID + ' ' + emailId + ' ' + parseInt(mobileNumber) + ' ' + today + ' ' + userName);
+            if ((Password != null && Password != '')) {
+                axios.post(Constants.url + 'UKP/rest/endpoints/UserRegistration', {
+                    "FirstName": firstName,
+                    "MiddleName": "MiddleName",
+                    "LastName": lastName,
+                    "RoleID": RoleID,
+                    "Level": "Level",
+                    "Email": emailId,
+                    "EmailConfirmed": 0,
+                    "PasswordHash": Password,
+                    "SecurityStamp": "SecurityStamp",
+                    "PhoneNumber": mobileNumber,
+                    "PhoneNumberConfirmed": 0,
+                    "TwoFactorEnabled": 0,
+                    "LockoutEndDateUtc": today,
+                    "LockoutEnabled": 0,
+                    "AccessFailedCount": 2,
+                    "UserName": userName,
+                    "VerificationCode": "12354",
+                    "VerificationCodeConfirmed": 0
+                }).then((res) => {
+                    if (res.data.status == true && res.data.statusCode == 200) {
+                        toast.success("Successfully Registered");
+                        cleraFiled();
+                    } else {
+                        toast.error("Failed");
+                    }
+                });
+            } else {
+                toast.error('Enter all the fields');
+            }
+        } else {
+            toast.error("Different password");
+        }
+    };
+
+    const {devAdmin, admin, dataEntry} = state;
+
+    return (
+        <div className="new-registraion-form">
+            <form className={
+                    classes.root
                 }
-                label="Dev Admin"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={admin}
-                    onChange={handleChange}
-                    name="admin"
-                  />
-                }
-                label="Admin"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={dataEntry}
-                    onChange={handleChange}
-                    name="dataEntry"
-                  />
-                }
-                label="Data Entry"
-              />
-            </FormGroup>
-          </FormControl>
+                noValidate
+                autoComplete="off">
+                <div>
+                    <TextField required id="firstName" label="First Name"/>
+                    <TextField required id="lastName" label="Last Name"/>
+                    <TextField required id="emailId" label="Email"/>
+                    <TextField id="mobileNumber" label="Mobile Number"/>
+                </div>
+                <div>
+                    <TextField required id="userName" label="User Name"/>
+                    <TextField required id="password" label="Password" type="password"/>
+                    <TextField required id="confirm-password" label="Confirm Password" type="password"/>
+                </div>
+                <div className="new-registraion-form-role">
+                    <h4>User Role</h4>
+                </div>
+                <div style={
+                    {margin: 8}
+                }>
+                    <form>
+                        <FormControl sx={
+                                {m: 3}
+                            }
+                            component="fieldset"
+
+                            variant="standard">
+                            <RadioGroup aria-label="quiz" name="quiz"
+                                value={value}
+                                onChange={handleRadioChange}>
+                                <FormControlLabel value="admin"
+                                    control={<Radio/>}
+                                    label="Admin"/>
+                                <FormControlLabel value="dataEntry"
+                                    control={<Radio/>}
+                                    label="Data Entry"/>
+                            </RadioGroup>
+                        </FormControl>
+                    </form>
+                </div>
+                <div style={
+                    {
+                        paddingLeft: "10px",
+                        marginBottom: "10px"
+                    }
+                }>
+                    <Button variant="contained" color="primary"
+                        onClick={
+                            handleIconClicks("submit")
+                    }>
+                        Save
+                    </Button>
+                </div>
+            </form>
+            <ToastContainer/>
         </div>
-        <div style={{ paddingLeft: "10px", marginBottom: "10px" }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleIconClicks("submit")}
-          >
-            Save
-          </Button>
-        </div>
-      </form>
-      <ToastContainer />
-    </div>
-  );
+    );
 }
